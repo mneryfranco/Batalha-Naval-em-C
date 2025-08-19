f7292ce27fc90c299de04fd61fc2672c596665f8
Mateus Nery Franco <neryfrancomateus@gmail.com>
2025-08-19 14:54:56 +0000

    Add funções: inicializarTabuleiro(), printTabuleiro(), addNavio() e buscaNavio()

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,12 +1,121 @@
-#include <stdio.h>
-
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+#include <stdio.h>
+#define maxLinha 10 // quantidade de linhas do tabuleiro
+#define maxColuna 10 // quantidade de colunas do tabuleiro
+int tabuleiro[maxLinha][maxColuna];
+
+void inicializarTabuleiro (){
+    for(int i=0; i<maxLinha; i++){
+        for (int j=0; j<maxLinha; j++){
+            tabuleiro[i][j] = 0;
+        }
+    }
+    return tabuleiro;
+}
+
+void printTabuleiro (){
+
+    for(int linha=-1; linha<maxLinha; linha++){
+        for (int coluna=-1; coluna<maxColuna; coluna++){
+            if(linha == -1){
+                if(coluna==-1) printf("\t");
+                else printf("\033[31m%d\033[0m\t", coluna);
+            }
+            else{
+                if(coluna == -1) printf("\033[31m%d\033[0m\t", linha);
+                else printf("%d\t", tabuleiro[linha][coluna]);
+            }
+            
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int buscaNavio(int tamNavio, int posX, int posY, int direcao){
+    int linha = posX, coluna = posY, i=0;
+    while (i < tamNavio){
+        if(tabuleiro[linha][coluna] == 1){
+            printf("Navio encontrado em (%d, %d)\n", linha, coluna);
+            return 1;
+        }
+        if (direcao == 1) linha++;
+        if (direcao == 2){
+            linha++;
+            coluna++;
+        }
+        if (direcao == 3) coluna++;
+        i++;
+    }
+    return 0;
+}
+
+void addNavio(int tamNavio, int posX, int posY, int direcao){
+    /*  
+    tamNavio: Tamanho do navio a ser adicionado no tabuleiro
+    posX: posição X inicial do navio
+    poxY: posição Y inicial do navio
+    Direção: 1 = Vertical | 2 = Diagonal | 3 = Horizontal 
+    */
+
+
+    //Tratamento de erros
+    //Se o usuário selecionar uma posição fora do tabuleiro, ou valores negativos, encerra.
+    if(posX<0 || posY<0 || posX>=maxColuna || posY>=maxLinha){
+        printf("Posicao inexistente no tabuleiro\n");
+        return;
+    } 
+    //Encerra pra valores invalidos no tamanho do navio
+    if(tamNavio <= 0){
+        printf("Tamanho do navio inválido\n");
+        return;
+    }
+    //Se o tamanho do barco exceder o tamanho do tabuleiro, encerra.
+    if(direcao==1 && posY+tamNavio-1 > maxLinha) {
+        printf("Erro: O Tamanho do navio excede o tabuleiro\n");
+        return;
+    } 
+    if(direcao==2 && (posX+tamNavio-1 >= maxColuna || posY+tamNavio-1 >= maxLinha)) {
+        printf("Erro: O Tamanho do navio excede o tabuleiro\n");
+        return;
+    } 
+    if(direcao==3 && posX+tamNavio-1 > maxColuna) {
+        printf("Erro: O Tamanho do navio excede o tabuleiro\n");
+        return;
+    } 
+    //Verifica se já existe um navio na posição
+    if(buscaNavio(tamNavio, posX, posY, direcao)){
+        return;
+    }
+
+
+    //Adiciona o navio no tabuleiro
+    int linha = posX, coluna = posY, i=0;
+    while (i < tamNavio){
+        tabuleiro[linha][coluna] = 1;
+        if (direcao == 1) linha++;
+        if (direcao == 2){
+            linha++;
+            coluna++;
+        }
+        if (direcao == 3) coluna++;
+        i++;
+    }
+}
+
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
+
+    inicializarTabuleiro();
+    addNavio(5, 1, 1, 1); // Add Navio tam=5, linha=1, coluna=1, Vertical
+    addNavio(4, 6, 6, 2); // Add Navio tam=4, linha=6, coluna=6, Diagonal
+    addNavio(3, 2, 5, 3); // Add Navio tam=3, linha=2, coluna=5, Horizontal
+    addNavio(3, 2, 5, 3); // Add Navio tam=3, linha=2, coluna=3, Horizontal
+    printTabuleiro();
+    
+    
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
